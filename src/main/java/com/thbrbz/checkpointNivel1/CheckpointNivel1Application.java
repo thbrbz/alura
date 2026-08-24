5088a84c8f1bd26a55c3a37fe2de50cbd55c2b0f
Thiago Barboza <thbrbz@gmail.com>
2026-08-24 16:43:00 -0300

    Inicial

---
+++ src/main/java/com/thbrbz/checkpointNivel1/CheckpointNivel1Application.java
@@ -0,0 +1,13 @@
+package com.thbrbz.checkpointNivel1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckpointNivel1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckpointNivel1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/thbrbz/checkpointNivel1/CheckpointNivel1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thbrbz.checkpointNivel1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckpointNivel1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
