744e6b0d1ac4ed31b0c7e97fd90af4c3197fb632
Thiago Barboza <thbrbz@gmail.com>
2026-08-27 00:37:13 -0300

    Adicionado validação para proibir reservar sala inativa.

---
+++ src/main/java/com/thbrbz/checkpointNivel1/validations/ValidaSeSalaInativa.java
@@ -0,0 +1,21 @@
+package com.thbrbz.checkpointNivel1.validations;
+
+import com.thbrbz.checkpointNivel1.dto.CriaReservaDto;
+import com.thbrbz.checkpointNivel1.entities.Sala;
+import com.thbrbz.checkpointNivel1.exceptions.ReservaException;
+import com.thbrbz.checkpointNivel1.services.SalaService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ValidaSeSalaInativa implements ReservaValidations{
+
+    @Autowired
+    private SalaService salaService;
+
+    @Override
+    public void validar(CriaReservaDto dto) {
+        Sala sala = salaService.buscar(dto.salaId());
+
+        if (!sala.isAtiva())
+            throw new ReservaException("Sala informada não pode ser usada por estar inativa!");
+    }
+}
