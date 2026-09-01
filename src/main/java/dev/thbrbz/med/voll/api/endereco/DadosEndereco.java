c695ee78ebf52576125245e43e8b5c1c2baee6ed
Thiago Barboza <thbrbz@gmail.com>
2026-08-31 21:11:52 -0300

    Aula 3: JPA, Validation, Lombok, Flyway.

--- src/main/java/dev/thbrbz/med/voll/api/endereco/DadosEndereco.java
+++ src/main/java/dev/thbrbz/med/voll/api/endereco/DadosEndereco.java
@@ -1,11 +1,14 @@
 package dev.thbrbz.med.voll.api.endereco;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public record DadosEndereco(
-        String logradouro,
-        String bairro,
-        String cep,
-        String cidade,
-        String uf,
+        @NotBlank String logradouro,
+        @NotBlank String bairro,
+        @NotBlank @Pattern(regexp = "\\d{8}") String cep,
+        @NotBlank String cidade,
+        @NotBlank String uf,
         String complemento,
         String numero
 ) {
---
+++ src/main/java/dev/thbrbz/med/voll/api/medico/MedicoRepository.java
@@ -0,0 +1,6 @@
+package dev.thbrbz.med.voll.api.medico;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
+}
