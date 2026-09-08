00b12c4c24032c44140b0a5098a606f30ee6a4d0
Thiago Barboza <thbrbz@gmail.com>
2026-09-08 16:57:01 -0300

    Aula 14: Testes automatizados.

---
+++ src/test/java/dev/thbrbz/med/voll/api/controller/MedicoControllerTest.java
@@ -0,0 +1,99 @@
+package dev.thbrbz.med.voll.api.controller;
+
+import dev.thbrbz.med.voll.api.domain.endereco.DadosEndereco;
+import dev.thbrbz.med.voll.api.domain.endereco.Endereco;
+import dev.thbrbz.med.voll.api.domain.medico.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+class MedicoControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private JacksonTester<DadosCadastroMedico> dadosCadastroMedicoJson;
+
+    @Autowired
+    private JacksonTester<DadosDetalhamentoMedico> dadosDetalhamentoMedicoJson;
+
+    @MockitoBean
+    private MedicoRepository repository;
+
+    @Test
+    @WithMockUser
+    @DisplayName("Deveria devolver codigo http 400 quando informacoes estao invalidas")
+    void cadastrar_cenario1() throws Exception {
+        var response = mvc
+                .perform(post("/medicos"))
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("Deveria devolver codigo http 201 quando informacoes estao validas")
+    void cadastrar_cenario2() throws Exception {
+        var dadosCadastro = new DadosCadastroMedico(
+                "Medico",
+                "61999999999",
+                "medico@voll.med",
+                "123456",
+                Especialidade.CARDIOLOGIA,
+                dadosEndereco());
+
+        when(repository.save(any())).thenReturn(new Medico(dadosCadastro));
+
+        var response = mvc
+                .perform(post("/medicos")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(dadosCadastroMedicoJson.write(dadosCadastro).getJson()))
+                .andReturn().getResponse();
+
+        var dadosDetalhamento = new DadosDetalhamentoMedico(
+                null,
+                dadosCadastro.nome(),
+                dadosCadastro.email(),
+                dadosCadastro.crm(),
+                dadosCadastro.telefone(),
+                dadosCadastro.especialidade(),
+                new Endereco(dadosCadastro.endereco())
+        );
+        var jsonEsperado = dadosDetalhamentoMedicoJson.write(dadosDetalhamento).getJson();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.getContentAsString()).isEqualTo(jsonEsperado);
+    }
+
+    private DadosEndereco dadosEndereco() {
+        return new DadosEndereco(
+                "rua xpto",
+                "bairro",
+                "00000000",
+                "Brasilia",
+                "DF",
+                null,
+                null
+        );
+    }
+}
