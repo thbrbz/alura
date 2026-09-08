a8462988b898157b870b991413ab565afa73ee3e
Thiago Barboza <thbrbz@gmail.com>
2026-09-08 16:57:01 -0300

    Aula 14: Testes automatizados.

---
+++ src/test/java/dev/thbrbz/med/voll/api/domain/medico/MedicoRepositoryTest.java
@@ -0,0 +1,37 @@
+package dev.thbrbz.med.voll.api.domain.medico;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class MedicoRepositoryTest {
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @Test
+    @DisplayName("Deveria devolver null quando unico medico cadastrado nao esta disponivel")
+    void escolherMedicoAleatorioLivreNaDataCenario1() {
+        var proximaSegundaAs10 = LocalDate.now()
+                .with(TemporalAdjusters.next(DayOfWeek.MONDAY))
+                .atTime(10, 0);
+
+        //Incompleto
+
+        var medicoLivre = medicoRepository.escolherMedicoAleatorioLivreNaData(Especialidade.CARDIOLOGIA, proximaSegundaAs10);
+
+        assertThat(medicoLivre).isNull();
+    }
+}
