224e7ee114a45589c500c60eee98f3fbc1e79e48
Thiago Barboza <thbrbz@gmail.com>
2026-09-14 01:27:51 -0300

    Aula 4: Implementação da entidade Usuario.

---
+++ src/main/java/med/voll/web_application/domain/usuario/UsuarioService.java
@@ -0,0 +1,20 @@
+package med.voll.web_application.domain.usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByEmailIgnoreCase(username)
+                .orElseThrow(() -> new UsernameNotFoundException("O usuário não foi encontrado!"));
+    }
+}
