47b1383d6d185badc57ca59c7587efc2799ce776
Thiago Barboza <thbrbz@gmail.com>
2026-09-24 22:19:42 -0300

    Aula 10: Implementando o "esqueci minha senha".

--- src/main/java/med/voll/web_application/domain/usuario/Usuario.java
+++ src/main/java/med/voll/web_application/domain/usuario/Usuario.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Collections;
 
@@ -18,6 +19,8 @@ public class Usuario  implements UserDetails {
     private String email;
     private String senha;
     private Boolean senhaAlterada;
+    private String token;
+    private LocalDateTime expiracaoToken;
 
     @Enumerated(EnumType.STRING)
     private Perfil perfil;
@@ -66,6 +69,22 @@ public class Usuario  implements UserDetails {
 
     public void setSenhaAlterada(Boolean senhaAlterada) {
         this.senhaAlterada = senhaAlterada;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public LocalDateTime getExpiracaoToken() {
+        return expiracaoToken;
+    }
+
+    public void setExpiracaoToken(LocalDateTime expiracaoToken) {
+        this.expiracaoToken = expiracaoToken;
     }
 
     public void alterarSenha(String senhaCriptografada) {
